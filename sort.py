2b901d3b9ecb21e853aee4ffb7d44fbe533c9a76
komakalyalni <36658769+komakalyalni@users.noreply.github.com>
2018-05-17 04:21:42 +0000

    Update sort.py

--- sort.py
+++ sort.py
@@ -5,8 +5,7 @@ try:
         for i in range(0,n):
             n1=input()
             list.append(n1)
-        print list
         list.sort()
-        print"sorted list is",list
+        print list
 except:
     print"Invalid Input"
